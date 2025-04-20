95b66671afa4086ce35296342ee969bf9e177fa6
MJ <wkdaudwn11@naver.com>
2025-04-20 21:18:34 +0900

    fix: SectionTitle 높이 안맞는 이슈 해결

--- src/components/shared/section-title/index.tsx
+++ src/components/shared/section-title/index.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 import SectionTitleItem from './section-title-item';
 
 const SectionTitle = ({ children }: PropsWithChildren) => (
-  <div className='hidden h-14 border-b border-slate-400 lg:sticky lg:top-14 lg:z-10 lg:flex lg:w-full lg:self-start lg:bg-gray-100 dark:border-slate-700 lg:dark:bg-slate-900'>
+  <div className='hidden h-[57px] border-b border-slate-400 lg:sticky lg:top-14 lg:z-10 lg:flex lg:w-full lg:self-start lg:bg-gray-100 dark:border-slate-700 lg:dark:bg-slate-900'>
     {children}
   </div>
 );
