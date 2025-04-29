bfe5df1f79918c5e40f92a8603797ccd6420d48c
MJ <wkdaudwn11@naver.com>
2025-04-30 08:28:39 +0900

    feat: 메인 페이지 스켈레톤 UI 구현 (#20)
    * feat: 메인 페이지 스켈레톤 UI 구현
    
    * refactor: 사용하지 않는 변수 삭제 및 eslint rule 수정
    
    * Refactor: 유지보수를 위해 스켈레톤 사이즈를 상수로 분리
    
    * feat: 반응형 Skeleton UI 작업 및 Skeleton 상수값, 타입 형식 변경
    
    * refactor: code-view 컴포넌트 Skeleton 상수값, 타입 재정의

--- src/app/(main)/_components/code-view.tsx
+++ src/app/(main)/_components/code-view.tsx
@@ -1,43 +1,172 @@
 import { FaGithub, FaEnvelope } from 'react-icons/fa';
 
+import { Skeleton } from '@/components/ui/skeleton';
+import { cn } from '@/lib/utils';
 import type { HelloResponse } from '@/types/hello';
+import type { Language } from '@/types/language';
+
+type SkeletonItem = {
+  base: string;
+  smHeight: string;
+  smWidth: Record<Language, string>;
+};
+
+type SkeletonSizeMap = {
+  title: SkeletonItem;
+  github: SkeletonItem;
+  email: SkeletonItem;
+  text01: SkeletonItem;
+  text02: SkeletonItem;
+};
+
+const SKELETON_SIZES: SkeletonSizeMap = {
+  title: {
+    base: 'h-[34px] w-full',
+    smHeight: 'sm:h-[34px]',
+    smWidth: {
+      ko: 'sm:w-[435px]',
+      en: 'sm:w-[460px]',
+    },
+  },
+  github: {
+    base: 'h-[24px] w-full',
+    smHeight: 'sm:h-[24px]',
+    smWidth: {
+      ko: 'sm:w-[397px]',
+      en: 'sm:w-[590px]',
+    },
+  },
+  email: {
+    base: 'h-[24px] w-full',
+    smHeight: 'sm:h-[24px]',
+    smWidth: {
+      ko: 'sm:w-[378px]',
+      en: 'sm:w-[590px]',
+    },
+  },
+  text01: {
+    base: 'h-[20px] w-2/3',
+    smHeight: 'sm:h-[20px]',
+    smWidth: {
+      ko: 'sm:w-[397px]',
+      en: 'sm:w-[530px]',
+    },
+  },
+  text02: {
+    base: 'h-[20px] w-1/2',
+    smHeight: 'sm:h-[20px]',
+    smWidth: {
+      ko: 'sm:w-[220px]',
+      en: 'sm:w-[370px]',
+    },
+  },
+};
 
 type Props = {
   data: HelloResponse;
+  isFetching: boolean;
+  language: Language;
 };
 
-const CodeView = ({ data }: Props) => (
-  <div className='flex flex-col justify-center gap-4 rounded-xl bg-white p-6 pt-14 shadow-md xl:h-[400px] dark:bg-slate-800'>
-    <h4 className='text-body-md-bold sm:text-heading-h5'>{data.code.title}!</h4>
-    <div className='space-y-4 sm:space-y-2'>
-      <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2'>
-        <FaGithub className='hidden sm:block' />
-        <span className='text-body-sm sm:text-body-md'>{data.code.github_text} 👉</span>
-        <a
-          href={process.env.NEXT_PUBLIC_GITHUB_URL}
-          className='text-body-sm underline underline-offset-4 transition-colors hover:text-teal-500 sm:text-body-md'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Github
-        </a>
+const CodeView = ({ data, isFetching, language }: Props) => {
+  const skeleton = SKELETON_SIZES;
+
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-col justify-center gap-4 rounded-xl bg-white p-6 shadow-md xl:h-[400px] dark:bg-slate-800',
+        !isFetching && 'pt-14'
+      )}
+    >
+      {isFetching ? (
+        <Skeleton
+          className={cn(
+            skeleton.title.base,
+            skeleton.title.smHeight,
+            skeleton.title.smWidth[language]
+          )}
+        />
+      ) : (
+        <h4 className='text-body-md-bold sm:text-heading-h5'>{data.code.title}</h4>
+      )}
+
+      <div className='space-y-4 sm:space-y-2'>
+        <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2'>
+          {isFetching ? (
+            <Skeleton
+              className={cn(
+                skeleton.github.base,
+                skeleton.github.smHeight,
+                skeleton.github.smWidth[language]
+              )}
+            />
+          ) : (
+            <>
+              <FaGithub className='hidden sm:block' />
+              <span className='text-body-sm sm:text-body-md'>{data.code.github_text}</span>
+              <a
+                href={process.env.NEXT_PUBLIC_GITHUB_URL}
+                className='text-body-sm underline underline-offset-4 transition-colors hover:text-teal-500 sm:text-body-md'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Github
+              </a>
+            </>
+          )}
+        </div>
+
+        <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2'>
+          {isFetching ? (
+            <Skeleton
+              className={cn(
+                skeleton.email.base,
+                skeleton.email.smHeight,
+                skeleton.email.smWidth[language]
+              )}
+            />
+          ) : (
+            <>
+              <FaEnvelope className='hidden sm:block' />
+              <span className='text-body-sm sm:text-body-md'>{data.code.email_text}</span>
+              <a
+                href='mailto:wkdaudwn1028@gmail.com'
+                className='text-body-sm underline underline-offset-4 transition-colors hover:text-teal-500 sm:text-body-md'
+              >
+                {data.code.email_button_text}
+              </a>
+            </>
+          )}
+        </div>
       </div>
-      <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2'>
-        <FaEnvelope className='hidden sm:block' />
-        <span className='text-body-sm sm:text-body-md'>{data.code.email_text} 📨</span>
-        <a
-          href='mailto:wkdaudwn1028@gmail.com'
-          className='text-body-sm underline underline-offset-4 transition-colors hover:text-teal-500 sm:text-body-md'
-        >
-          {data.code.email_button_text}
-        </a>
+
+      <div className='space-y-1'>
+        {isFetching ? (
+          <>
+            <Skeleton
+              className={cn(
+                skeleton.text01.base,
+                skeleton.text01.smHeight,
+                skeleton.text01.smWidth[language]
+              )}
+            />
+            <Skeleton
+              className={cn(
+                skeleton.text02.base,
+                skeleton.text02.smHeight,
+                skeleton.text02.smWidth[language]
+              )}
+            />
+          </>
+        ) : (
+          <>
+            <p className='text-body-sm text-gray-600 dark:text-gray-300'>{data.code.text01}</p>
+            <p className='text-body-sm text-gray-500 italic'>&quot;{data.code.text02}&quot;</p>
+          </>
+        )}
       </div>
     </div>
-    <div className='space-y-1'>
-      <p className='text-body-sm text-gray-600 dark:text-gray-300'>{data.code.text01}</p>
-      <p className='text-body-sm text-gray-500 italic'>&quot;{data.code.text02}&quot;</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CodeView;
--- src/app/(main)/_components/main.tsx
+++ src/app/(main)/_components/main.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { MdCode } from 'react-icons/md';
 
@@ -10,16 +10,47 @@ import ErrorContent from '@/components/shared/error-content';
 import FadeInUp from '@/components/shared/fade-in-up';
 import Typewriter from '@/components/shared/typewriter';
 import { Label } from '@/components/ui/label';
+import { Skeleton } from '@/components/ui/skeleton';
+import Spinner from '@/components/ui/spinner';
 import { Toggle } from '@/components/ui/toggle';
 import { fetchHello } from '@/lib/api/hello';
 import { cn } from '@/lib/utils';
 import type { HelloResponse } from '@/types/hello';
+import type { Language } from '@/types/language';
 
 import CodeView from './code-view';
 import SnakeGame from './snake-game';
 
 type Props = {
   initialData: HelloResponse;
+};
+
+type SkeletonSize = {
+  line1: { base: string; sm?: string };
+  line2: { base: string; smHeight: string; smWidth: Record<Language, string> };
+  line3: { base: string; smHeight: string; smWidth: Record<Language, string> };
+};
+
+const SKELETON_SIZES: SkeletonSize = {
+  line1: {
+    base: 'h-[24px] w-[130px] sm:h-[27px]',
+  },
+  line2: {
+    base: 'h-[46.8px] w-full',
+    smHeight: 'sm:h-[66px]',
+    smWidth: {
+      ko: 'sm:w-[383.41px]',
+      en: 'sm:w-[542px]',
+    },
+  },
+  line3: {
+    base: 'h-[33.59px] w-[261.61px]',
+    smHeight: 'sm:h-[41px]',
+    smWidth: {
+      ko: 'sm:w-[261.61px]',
+      en: 'sm:w-[380px]',
+    },
+  },
 };
 
 const Main = ({ initialData }: Props) => {
@@ -44,7 +75,7 @@ import { FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const Hello = () => (
   <div className='flex h-[400px] flex-col justify-center gap-4 rounded-xl bg-white p-6 pt-14 shadow-md dark:bg-slate-800'>
-    <h4 className='text-heading-h6 sm:text-heading-h5'>${data.code.title}!</h4>
+    <h4 className='text-heading-h6 sm:text-heading-h5'>${data.code.title}</h4>
     <div className='space-y-4 sm:space-y-2'>
       <div className='flex flex-col sm:flex-row sm:items-center sm:gap-2'>
         <FaGithub className='hidden sm:block' />
@@ -98,44 +129,76 @@ export default Hello;
     [data]
   );
 
-  if (isFetching) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    setIsShowCodeHighlight(false);
+  }, [language]);
 
   if (isError) {
     return <ErrorContent />;
   }
 
   return (
-    <div className='flex h-full flex-col items-center justify-center gap-5 px-4 py-10 xl:gap-8'>
-      <div className='h-[110px] sm:h-[139px]'>
+    <div
+      className={cn(
+        'flex h-full flex-col items-center justify-center gap-5 px-4 py-10 xl:gap-8',
+        isFetching && 'gap-8'
+      )}
+    >
+      <div className='h-[110px] w-full sm:h-[139px] sm:w-auto'>
         <FadeInUp>
-          <Typewriter lines={lines} isLoop />
+          {isFetching ? (
+            <div className='flex flex-col gap-2'>
+              <Skeleton className={SKELETON_SIZES.line1.base} />
+              <Skeleton
+                className={cn(
+                  SKELETON_SIZES.line2.base,
+                  SKELETON_SIZES.line2.smHeight,
+                  SKELETON_SIZES.line2.smWidth[language as Language]
+                )}
+              />
+              <Skeleton
+                className={cn(
+                  SKELETON_SIZES.line3.base,
+                  SKELETON_SIZES.line3.smHeight,
+                  SKELETON_SIZES.line3.smWidth[language as Language]
+                )}
+              />
+            </div>
+          ) : (
+            <Typewriter lines={lines} isLoop />
+          )}
         </FadeInUp>
       </div>
       <div className='flex w-full items-center justify-center gap-5 xl:flex-row'>
-        <div className={cn('lg:w-fit', isShowCodeHighlight && 'w-full')}>
+        <div className={cn('w-full lg:w-fit')}>
           <FadeInUp>
-            <div className='relative'>
-              <Toggle
-                aria-label='Toggle italic'
-                className='absolute top-2 right-2 z-10'
-                pressed={isShowCodeHighlight}
-                onPressedChange={() => setIsShowCodeHighlight(prev => !prev)}
-              >
-                <MdCode />
-                <Label className='cursor-pointer'>code</Label>
-              </Toggle>
+            <div className='relative w-full'>
+              {!isFetching && (
+                <Toggle
+                  aria-label='Toggle italic'
+                  className='absolute top-2 right-2 z-10'
+                  pressed={isShowCodeHighlight}
+                  onPressedChange={() => setIsShowCodeHighlight(prev => !prev)}
+                >
+                  <MdCode />
+                  <Label className='cursor-pointer'>code</Label>
+                </Toggle>
+              )}
               {isShowCodeHighlight ? (
                 <CodeHighlight rawCode={rawCode} loadingClassName='w-full sm:w-[548px]' />
               ) : (
-                <CodeView data={data} />
+                <CodeView data={data} isFetching={isFetching} language={language as Language} />
               )}
             </div>
           </FadeInUp>
         </div>
-        <div className='hidden items-center justify-center xl:flex'>
+        <div className='relative hidden items-center justify-center xl:flex'>
           <FadeInUp>
+            {isFetching && (
+              <div className='absolute z-10 flex h-full w-full items-center justify-center rounded-xl bg-black/50'>
+                <Spinner size='md' />
+              </div>
+            )}
             <SnakeGame />
           </FadeInUp>
         </div>
