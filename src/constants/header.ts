d274c07277cc44417416fd1a268449a7c530288d
myungjuice <wkdaudwn1028@gmail.com>
2025-07-27 19:41:00 +0900

    fix: framer-motion transition ease 타입 에러 수정

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -13,12 +13,12 @@ export const overlayAnimation = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { duration: 0.3, ease: 'easeInOut' },
+  transition: { duration: 0.3 },
 };
 
 export const menuAnimation = {
   initial: { opacity: 0, y: -30 },
   animate: { opacity: 1, y: 0 },
   exit: { opacity: 0, y: -30 },
-  transition: { duration: 0.3, ease: 'easeInOut' },
+  transition: { duration: 0.3 },
 };
